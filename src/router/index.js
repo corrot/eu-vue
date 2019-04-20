6815634ccaecf6db5b60fafb9317a3bc56adb9fe
Giorgi Koroshinadze <gkoroshinadze@gmail.com>
2019-04-20 22:40:23 +0400

    add releases page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Home from '@/pages/Home/Home';
 import AboutUs from '@/pages/AboutUs/AboutUs';
 import Contact from '@/pages/Contact/Contact';
+import Releases from '@/pages/Releases/Releases';
 
 Vue.use(Router);
 
@@ -228,6 +229,14 @@ export const routes = [
     name: 'contact',
     component: Contact,
     children: [],
+  },
+  {
+    title: 'Releases',
+    path: '/releases/:id',
+    name: 'releases',
+    component: Releases,
+    children: [],
+    hidden: true,
   },
 ];
 
