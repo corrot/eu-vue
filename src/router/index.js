a62e61d920945c1048ac1a22f4f2de1990468e26
Giorgi Koroshinadze <gkoroshinadze@gmail.com>
2019-04-24 01:01:31 +0400

    change page import names to Uppercase

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import Home from '@/pages/Home/Home';
-import AboutUs from '@/pages/AboutUs/AboutUs';
-import Contact from '@/pages/Contact/Contact';
-import Releases from '@/pages/Releases/Releases';
-import PageNotFound from '@/pages/PageNotFound/PageNotFound';
+import Home from '@/Pages/Home/Home';
+import AboutUs from '@/Pages/AboutUs/AboutUs';
+import Contact from '@/Pages/Contact/Contact';
+import Releases from '@/Pages/Releases/Releases';
+import PageNotFound from '@/Pages/PageNotFound/PageNotFound';
 
 Vue.use(Router);
 
