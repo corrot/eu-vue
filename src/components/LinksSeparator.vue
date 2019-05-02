b10325b0233e85cc055cb3761ac443f7ed68beda
Giorgi Koroshinadze <gkoroshinadze@gmail.com>
2019-05-02 19:05:38 +0400

    create separator component for links

---
+++ src/components/LinksSeparator.vue
@@ -0,0 +1,17 @@
+<template>
+  <span class="links-separator"></span>
+</template>
+
+<script>
+export default {
+  name: 'LinksSeparator',
+};
+</script>
+
+<style lang="postcss" scoped>
+.links-separator {
+  height: 14px;
+  border-left: rgba(0, 0, 0, 0.2) 1px solid;
+  margin: 0 10px 0 4px;
+}
+</style>
