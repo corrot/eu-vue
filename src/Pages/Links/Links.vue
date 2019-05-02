57d48793168c3c9bff75f98b51abb20b5eab0b43
Giorgi Koroshinadze <gkoroshinadze@gmail.com>
2019-05-02 19:04:41 +0400

    add links page

---
+++ src/Pages/Links/Links.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Links</h1>
+    <ul class="links container">
+      <li v-for="link in links" :key="link.id">
+        <a :href="link.path">{{ link.title }}</a>
+        <div>{{ link.description }}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const links = [
+  {
+    id: 0,
+    title: 'example title 1',
+    path: 'https://example.net',
+    description:
+      'example description that`s supposed to be long enough to run on a new line. Is not it already? Okay - Alright! Let`s go on typing some random stuff... I think it`s alright for now!',
+  },
+  {
+    id: 1,
+    title: 'example title 2',
+    path: 'https://example.net',
+    description: 'example description',
+  },
+];
+
+export default {
+  name: 'Links',
+  data() {
+    return {
+      links,
+    };
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>
