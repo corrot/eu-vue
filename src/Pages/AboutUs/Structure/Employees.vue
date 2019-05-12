dc47cf97f68b7b88514a24d5cb29c4225d132261
Giorgi Koroshinadze <gkoroshinadze@gmail.com>
2019-05-12 18:30:50 +0400

    add structure page, 5 hours work

---
+++ src/Pages/AboutUs/Structure/Employees.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-container>
+    <div style="padding: 15px 0">
+      <h3>{{item["title"]}}</h3>
+      <h4>{{item["name"]}}</h4>
+      <div>
+        tel:
+        <a :href="'tel:' + item['tel']">{{item['tel']}}</a>
+      </div>
+      <div>
+        email:
+        <div v-for="(mail, index) in item.email" :key="index">
+          <a :href="'mailto:' + mail">{{mail}}</a>
+        </div>
+      </div>
+    </div>
+  </b-container>
+</template>
+
+<script>
+import i18n from '@/plugins/i18n';
+import tree from './structure.js';
+
+export default {
+  name: 'WhatWeDo',
+  data() {
+    return {
+      tree,
+      item: tree.filter(i => i.id == this.$route.params.id)[0],
+    };
+  },
+  computed: {
+    locale: () => {
+      console.log(i18n.locale);
+      return i18n.locale;
+    },
+  },
+  mounted() {},
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>
---
+++ src/Pages/AboutUs/Structure/Structure.vue
@@ -0,0 +1,37 @@
+<template>
+  <b-container>
+    <div :v-if="locale">
+      <img style="width: 100%" :src="structure" alt="Structure">
+      <div v-for="(item, index) in tree" :key="index" class="mb-2">
+        <b-button :to="'/employees/' + index" :id="index">{{ item.title }}</b-button>
+        <b-tooltip :target="index">{{ item.name }}</b-tooltip>
+      </div>
+    </div>
+  </b-container>
+</template>
+
+<script>
+import i18n from '@/plugins/i18n';
+import structure from './structure.jpg';
+import tree from './structure.js';
+
+export default {
+  name: 'WhatWeDo',
+  data() {
+    return {
+      structure,
+      tree,
+    };
+  },
+  computed: {
+    locale: () => {
+      console.log(i18n.locale);
+      return i18n.locale;
+    },
+  },
+  mounted() {},
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>
