d868906a0b24256408027d04495d81c63bd6c7c0
OfeliaMuradova <ofelia.muradovi@gmail.com>
2019-05-16 19:11:14 +0400

    Sitemap styles, 30mins work

--- src/Pages/Sitemap/Sitemap.vue
+++ src/Pages/Sitemap/Sitemap.vue
@@ -1,17 +1,18 @@
 <template>
   <b-container id="sitemap-container">
+    <h1 id="sitemap-title">Sitemap</h1>
     <div v-for="route in routes" :key="route.name" class="route-title">
-      
-      <div :text="$t(route.title)" v-if="!route.children.length">
-        <router-link :to="route.path">{{ $t(route.title) }}</router-link>
-      </div>
-      <div v-if="route.children.length">
-        <router-link :to="route.path">{{ $t(route.title) }}</router-link>
-        <div v-for="item in route.children" :key="item.name" class="child-title">
-
-          <router-link :to="item.path">{{ $t(item.title) }}</router-link>
-        </div>
-      </div>
+        <span class="dot"></span>
+        <span :text="$t(route.title)" v-if="!route.children.length">
+          <router-link :to="route.path" class="dark-blue">{{ $t(route.title) }}</router-link>
+        </span>
+        <span v-if="route.children.length">
+          <router-link :to="route.path" class="dark-blue">{{ $t(route.title) }}</router-link><br/>
+            <span v-for="item in route.children" :key="item.name" class="child-title">
+              <router-link :to="item.path" class="light-blue">{{ $t(item.title) }}</router-link>
+              <br/>
+            </span>
+        </span>
     </div>
   </b-container>
 </template>
@@ -31,19 +32,51 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
+
+#sitemap-container{
+  padding: 2% 0 2% 4%;
+}
+
+#sitemap-title{
+  margin-bottom: 30px;
+  letter-spacing: 2px;
+}
+
 .route-title {
   text-align: left;
   font-weight: 600;
-  margin: 10px;
+  line-height: 32px;
 }
 
 .child-title {
   margin: 5px;
   margin-left: 30px;
   font-weight: 400;
+  padding-left: 8px;
 }
 
-#sitemap-container{
-  padding: 1% 0;
+.dark-blue{
+  color: #2c3e50;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-size: 18px;
 }
+
+.dark-blue:hover{
+  color: #527496;
+}
+
+.light-blue{
+  color: #4b6580;
+  font-size: 17px;
+  border-left: 1px solid grey;
+  border-bottom: 1px solid grey;
+  padding-left: 8px;
+  padding-bottom: 4px;
+}
+
+.light-blue:hover{
+  color: #6c99c7;
+}
+
 </style>
