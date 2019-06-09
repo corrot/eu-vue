d73dfd130d8415b7d9642e04ea0af33cc855f83a
OfeliaMuradova <ofelia.muradovi@gmail.com>
2019-06-09 20:26:49 +0400

    one more attempt

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');	const express = require('express');
 var path = require('path');	const path = require('path');
-var serveStatic = require('serve-static');	const history = require('connect-history-api-fallback');
-app = express();	
- app.use(serveStatic(__dirname + '/dist'));	const app = express();
+var serveStatic = require('serve-static');	
+const history = require('connect-history-api-fallback');
+const app = express();
+app.use(history());
+// app.use(serveStatic(__dirname + '/dist'));	
 var port = process.env.PORT || 5000;	
- app.listen(port);	const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+app.listen(port);
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 console.log('server started ' + port);
