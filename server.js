0f917dd7fee3b1c5f76d511d9d31ec7ee30209d8
OfeliaMuradova <ofelia.muradovi@gmail.com>
2019-06-09 19:07:50 +0400

    server.js workaround

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-const express = require('express');
-const path = require('path');
-const history = require('connect-history-api-fallback');
+const http = require('http')
+const fs = require('fs')
+const httpPort = 80
 
-const app = express();
+http.createServer((req, res) => {
+  fs.readFile('index.htm', 'utf-8', (err, content) => {
+    if (err) {
+      console.log('We cannot open "index.htm" file.')
+    }
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    })
 
-app.use(staticFileMiddleware);
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
-app.use(staticFileMiddleware);
-
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
-});
-
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
+    res.end(content)
+  })
+}).listen(httpPort, () => {
+  console.log('Server listening on: http://localhost:%s', httpPort)
 });
