9a399ef6198bd4318f9f036e9da8700deabb9ec9
gkoroshinadze <gkoroshinadze@gmail.com>
2019-06-10 21:13:25 +0400

    asd

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-// server.js
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-//added this middleware
-var history = require('connect-history-api-fallback');
-
-app = express()
-//started middleware
-app.use(history());
-
-app.use(serveStatic(__dirname))
-var port = process.env.PORT || 5000
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+// create the express app
+const app = express()
+// create middleware to handle the serving the app
+app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
+// Catch all routes and redirect to the index file
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html')
+})
+// Create default port to serve the app on
+const port = process.env.PORT || 5000
 app.listen(port)
-console.log('server started ' + port)
+// Log to feedback that this is actually running
+console.log('Server started on port ' + port)
