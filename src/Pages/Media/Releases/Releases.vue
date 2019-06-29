908620bdf4e69242b5d0855c3fe80cf09dcee18a
Giorgi Koroshinadze <gkoroshinadze@gmail.com>
2019-06-29 18:00:44 +0400

    pressReleases: view document, view image, swap places. 2 hours

--- src/Pages/Media/Releases/Releases.vue
+++ src/Pages/Media/Releases/Releases.vue
@@ -2,43 +2,75 @@
   <b-container>
     <loading-spinner v-if="loading"/>
     <server-error v-if="errored"/>
-      <div v-if="!errored && !loading">
-        <div role="tablist" class="mb-5" v-if="pressReleases.length">
-        <h5 class="section-title">{{ $t('CAGInPress') }}</h5>
-          <b-card no-body class="mb-2" v-for="internship in pressReleases" :key="internship.id">
-            <b-card-header header-tag="header" class="p-3" role="tab">
-              <a block href="#" v-b-toggle="'accordion-2-' + internship.id" variant="info">
-                <span>{{ internship[`title_${locale}`] }}</span>
-                <font-awesome-icon class="mr-1" :icon="['fas', 'fa-plus']"/>
-              </a>
-            </b-card-header>
-            <b-collapse :id="`accordion-2-${internship.id}`" accordion="my-accordion" role="tabpanel">
-              <b-card-body>
-                <b-card-text><vue-markdown class="article">{{ internship[`article_${locale}`] }}</vue-markdown></b-card-text>
-                <b-button :href="internship[`document_${locale}`] && `${API_BASE_URL}/uploads/${internship[`document_${locale}`].hash}${internship[`document_${locale}`].ext}`" target="_blank">{{ $t('ViewDocument') }}</b-button>
-              </b-card-body>
-            </b-collapse>
-          </b-card>
-        </div>
-
-          <h5 class="section-title">{{ $t('PressReleases') }}</h5>
-        <div role="tablist" class="mb-5" v-if="cagInPress.length">
-          <b-card no-body class="mb-2" v-for="vacancy in cagInPress" :key="vacancy.id">
-            <b-card-header header-tag="header" class="p-3" role="tab">
-              <a block href="#" v-b-toggle="'accordion-1-' + vacancy.id" variant="info">
-                <span>{{ vacancy[`title_${locale}`] }}</span>
-                <font-awesome-icon class="mr-1" :icon="['fas', 'fa-plus']"/>
-              </a>
-            </b-card-header>
-            <b-collapse :id="`accordion-1-${vacancy.id}`" accordion="my-accordion" role="tabpanel">
-              <b-card-body>
-                <b-card-text><vue-markdown class="article">{{ vacancy[`article_${locale}`] }}</vue-markdown></b-card-text>
-                <b-button :href="vacancy[`document_${locale}`] && `${API_BASE_URL}/uploads/${vacancy[`document_${locale}`].hash}${vacancy[`document_${locale}`].ext}`" target="_blank">{{ $t('ViewDocument') }}</b-button>
-              </b-card-body>
-            </b-collapse>
-          </b-card>
-        </div>
+    <div v-if="!errored && !loading">
+      <div role="tablist" class="mb-5" v-if="cagInPress.length">
+        <h5 class="section-title">{{ $t('PressReleases') }}</h5>
+        <b-card no-body class="mb-2" v-for="vacancy in cagInPress" :key="vacancy.id">
+          <b-card-header header-tag="header" class="p-3" role="tab">
+            <a
+              block
+              :href="'#accordion-1-' + vacancy.id"
+              v-b-toggle="'accordion-1-' + vacancy.id"
+              variant="info"
+            >
+              <span>{{ vacancy[`title_${locale}`] }}</span>
+              <font-awesome-icon class="mr-1" :icon="['fas', 'fa-plus']"/>
+            </a>
+          </b-card-header>
+          <b-collapse :id="`accordion-1-${vacancy.id}`" accordion="my-accordion" role="tabpanel">
+            <b-card-body>
+              <img
+                class="mb-3 mt-3"
+                style="max-width: 100%"
+                :src="vacancy.image && `${API_BASE_URL}/uploads/${vacancy.image.hash}${vacancy.image.ext}`"
+              >
+              <b-card-text>
+                <vue-markdown class="article">{{ vacancy[`article_${locale}`] }}</vue-markdown>
+              </b-card-text>
+              <b-button
+                v-if="vacancy[`doc_${locale}`].length"
+                :href="vacancy[`doc_${locale}`] && `${API_BASE_URL}/uploads/${vacancy[`doc_${locale}`][0].hash}${vacancy[`doc_${locale}`][0].ext}`"
+                target="_blank"
+              >{{ $t('ViewDocument') }}</b-button>
+            </b-card-body>
+          </b-collapse>
+        </b-card>
       </div>
+
+      <div role="tablist" class="mb-5" v-if="pressReleases.length">
+        <h5 class="section-title">{{ $t('CAGInPress') }}</h5>
+        <b-card no-body class="mb-2" v-for="internship in pressReleases" :key="internship.id">
+          <b-card-header header-tag="header" class="p-3" role="tab">
+            <a
+              block
+              :href="'#accordion-2-' + internship.id"
+              v-b-toggle="'accordion-2-' + internship.id"
+              variant="info"
+            >
+              <span>{{ internship[`title_${locale}`] }}</span>
+              <font-awesome-icon class="mr-1" :icon="['fas', 'fa-plus']"/>
+            </a>
+          </b-card-header>
+          <b-collapse :id="`accordion-2-${internship.id}`" accordion="my-accordion" role="tabpanel">
+            <b-card-body>
+              <img
+                class="mb-3 mt-3"
+                style="max-width: 100%"
+                :src="internship.image && `${API_BASE_URL}/uploads/${internship.image.hash}${internship.image.ext}`"
+              >
+              <b-card-text>
+                <vue-markdown class="article">{{ internship[`article_${locale}`] }}</vue-markdown>
+              </b-card-text>
+              <b-button
+                v-if="internship[`doc_${locale}`].length"
+                :href="internship[`doc_${locale}`] && `${API_BASE_URL}/uploads/${internship[`doc_${locale}`][0].hash}${internship[`doc_${locale}`][0].ext}`"
+                target="_blank"
+              >{{ $t('ViewDocument') }}</b-button>
+            </b-card-body>
+          </b-collapse>
+        </b-card>
+      </div>
+    </div>
   </b-container>
 </template>
 
@@ -58,7 +90,7 @@ export default {
       errored: false,
       API_BASE_URL,
       pressReleases: null,
-      cagInPress: null
+      cagInPress: null,
     };
   },
   computed: {
@@ -71,13 +103,14 @@ export default {
       .get(RELEASES_URL)
       .then(response => {
         this.pressReleases = response.data.slice(0, 4);
+        console.log(this.pressReleases.map(o => o.doc_ge));
       })
       .catch(error => {
         console.log(error);
         this.errored = true;
       })
       .finally(() => (this.loading = false));
-      
+
     this.$http
       .get(PRESSRELEASES_URL)
       .then(response => {
@@ -94,5 +127,4 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
-
 </style>
