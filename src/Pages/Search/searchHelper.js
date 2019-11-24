d1eea051bb14e7905743ed16a99716bec4f1af1c
Giorgi Koroshinadze <gkoroshinadze@gmail.com>
2019-11-24 23:48:11 +0400

    search improvement 5

--- src/Pages/Search/searchHelper.js
+++ src/Pages/Search/searchHelper.js
@@ -1,100 +1,51 @@
 import {
-  NEWSARCHIVE_URL,
   EVENTS_URL,
   PRESSRELEASES_URL,
   PROACTIVE_INFORMATION_ARCHIVE,
   ANNOUNCEMENTS_URL,
   DECISIONS_URL,
   EMPLOYEES_URL,
-  // ABUSEOFDOMINITIONBYPROHIBITION_URL,
-  // ABUSEOFDOMINITIONBYINVESTIGATION_URL,
-  // AntiCompetitiveAgreementsByProhibition_URL,
-  // AntiCompetitiveAgreementsByInvestigation_URL,
-  // DistortionOfCompetitionByStateAuthoritiesByProhibition_URL,
-  // DistortionOfCompetitionByStateAuthoritiesByInvestigation_URL,
-  // UnfairCompetitionByProhibition_URL,
-  // UnfairCompetitionByInvestigation_URL,
-  // ConcentrationControl_URL,
-  // StateAid_URL,
-  // MarketMonitoring_URL,
+  PROJECTS_URL,
+  COOPERATION_URL,
+  PUBLICATIONS_URL,
 } from '@/constants.js';
 
 const endpoints = [
-  // { redirectTo: 'abuseOfDominantPositionByProhibition', url: ABUSEOFDOMINITIONBYPROHIBITION_URL, fields: ['title', 'article', 'tags'] },
-  // { redirectTo: 'abuseOfDominantPositionByInvestigation', url: ABUSEOFDOMINITIONBYINVESTIGATION_URL, fields: ['title', 'article', 'tags'] },
-  // { redirectTo: 'antiCompetitiveAgreementsByProhibition', url: AntiCompetitiveAgreementsByProhibition_URL, fields: ['title', 'article', 'tags'] },
-  // { redirectTo: 'antiCompetitiveAgreementsByInvestigation', url: AntiCompetitiveAgreementsByInvestigation_URL, fields: ['title', 'article', 'tags'] },
-  // { redirectTo: 'distortionOfCompetitionByStateAuthoritiesByProhibition', url: DistortionOfCompetitionByStateAuthoritiesByProhibition_URL, fields: ['title', 'article', 'tags'] },
-  // { redirectTo: 'distortionOfCompetitionByStateAuthoritiesByInvestigation', url: DistortionOfCompetitionByStateAuthoritiesByInvestigation_URL, fields: ['title', 'article', 'tags'] },
-  // { redirectTo: 'unfairCompetitionByProhibition', url: UnfairCompetitionByProhibition_URL, fields: ['title', 'article', 'tags'] },
-  // { redirectTo: 'unfairCompetitionByInvestigation', url: UnfairCompetitionByInvestigation_URL, fields: ['title', 'article', 'tags'] },
-  // { redirectTo: 'concentrationControl', url: ConcentrationControl_URL, fields: ['title', 'article', 'tags'] },
-  // { redirectTo: 'stateAid', url: StateAid_URL, fields: ['title', 'article', 'tags'] },
-  // { redirectTo: 'marketMonitoring', url: MarketMonitoring_URL, fields: ['title', 'article', 'tags'] },
-  { redirectTo: 'decisionDetails', url: DECISIONS_URL, fields: ['tags', 'title', 'article'] },
   {
-    redirectTo: 'eventDetails',
+    url: DECISIONS_URL,
+    fields: ['tags', 'title', 'article'],
+  },
+  {
     url: EVENTS_URL,
     fields: ['title', 'article'],
   },
   {
-    redirectTo: 'pressReleasesDetail',
     url: PRESSRELEASES_URL,
     fields: ['title', 'article'],
   },
   {
-    redirectTo: 'proactiveInformationDetails',
     url: PROACTIVE_INFORMATION_ARCHIVE,
     fields: ['title', 'comment'],
   },
   {
-    redirectTo: 'announcementDetails',
     url: ANNOUNCEMENTS_URL,
     fields: ['title', 'article'],
   },
   {
-    redirectTo: 'employDetail',
     url: EMPLOYEES_URL,
     fields: ['name', 'biography'],
   },
-];
-
-const flat = [
   {
-    title: 'Decisions',
-    path: '/decisions/:id',
-    name: 'decisionDetails',
-    hidden: true,
+    url: PROJECTS_URL,
+    fields: ['title', 'text'],
   },
   {
-    title: 'Events',
-    path: '/media/events/:id',
-    name: 'eventDetails',
-    hidden: true,
+    url: COOPERATION_URL,
+    fields: ['title', 'text'],
   },
   {
-    title: 'PressReleasesDetail',
-    path: '/media/press-releases/:id',
-    name: 'pressReleasesDetail',
-    hidden: true,
-  },
-  {
-    title: 'ProactiveInformationDetails',
-    path: '/public/proactive-information-details/:id',
-    name: 'proactiveInformationDetails',
-    hidden: true,
-  },
-  {
-    title: 'Announcement',
-    path: '/media/announcement/:id',
-    name: 'announcementDetails',
-    hidden: true,
-  },
-  {
-    title: 'Structure',
-    path: '/about-us/employ-detail/:id',
-    name: 'employDetail',
-    hidden: true,
+    url: PUBLICATIONS_URL,
+    fields: ['article', 'title'],
   },
 ];
 
@@ -107,7 +58,6 @@ export const searchEndpoints = (locale, query) => {
   endpoints.forEach(e => {
     return e.fields.map(o => {
       const j = {
-        name: flat.find(o => e.redirectTo == o.name).title,
         link: `${e.url}?${getPath(o, locale, query.toString())}&_limit=1000`,
       };
       arr.push(j);
