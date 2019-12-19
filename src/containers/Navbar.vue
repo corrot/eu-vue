628f88f8d777531a5e836f1ab1d462baf18279ab
Giorgi Koroshinadze <gkoroshinadze@gmail.com>
2019-12-20 02:41:31 +0400

    remove navbar from mobile version, add routes to sideba, 5 hours work

--- src/containers/Navbar.vue
+++ src/containers/Navbar.vue
@@ -1,33 +1,65 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="dark" :sticky="true" class="navbar">
-    <b-container style="height: 3em;">
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <div v-for="route in routes" :key="route.name" class="main-nav-item">
-            <b-nav-item :text="$t(route.title)" v-if="!route.children.length && !route.hidden">
-              <router-link :to="route.path">
-                <span
-                  :class="activePath === route.path ? 'navbar-title active' : 'navbar-title'"
-                >{{ $t(route.title) }}</span>
-              </router-link>
-            </b-nav-item>
-            <b-nav-item-dropdown v-if="route.children.length && !route.hidden">
-              <template slot="button-content">
-                <span :class="activePath === route.path ? 'navbar-title active' : 'navbar-title'">{{ $t(route.title) }}</span>
-              </template>
-              <b-dropdown-item class="dropdown-override" v-for="(item, index) in route.children" :key="index" :to="item.path" v-if="!item.hidden">
-                <span v-if="!item.hidden">{{ $t(item.title) }}
-                  <!-- <b-dropdown-item class="dropdown-override" :to="subItem.path" v-for="(subItem, index) in item.children" :key="index">{{ $t(subItem.title) }}
-                  </b-dropdown-item> -->
-                </span>
-              </b-dropdown-item>
-            </b-nav-item-dropdown>
-          </div>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-container>
-  </b-navbar>
+  <div>
+    <div
+      class="btn btn-primary mobile"
+      @click="toggleSidebar()"
+      style="position: absolute; top: 12px; left: 10px"
+    >></div>
+    <div :class="`sidebar mobile ${isSidebarOn ? 'show' : ''}`">
+      <div v-for="route in routes" :key="route.name">
+        <b-dropdown :text="$t(route.title)" v-if="!route.hidden">
+          <b-dropdown-item
+            class="dropdown-override"
+            v-for="(item, index) in route.children"
+            :key="index"
+            :to="item.path"
+            v-if="!item.hidden"
+          >
+            <span v-if="!item.hidden">{{ $t(item.title) }}</span>
+          </b-dropdown-item>
+        </b-dropdown>
+      </div>
+    </div>
+
+    <b-navbar toggleable="lg" type="dark" variant="dark" :sticky="true" class="navbar">
+      <b-container style="height: 3em;">
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <div v-for="route in routes" :key="route.name" class="main-nav-item">
+              <b-nav-item :text="$t(route.title)" v-if="!route.children.length && !route.hidden">
+                <router-link :to="route.path">
+                  <span
+                    :class="activePath === route.path ? 'navbar-title active' : 'navbar-title'"
+                  >{{ $t(route.title) }}</span>
+                </router-link>
+              </b-nav-item>
+              <b-nav-item-dropdown v-if="route.children.length && !route.hidden">
+                <template slot="button-content">
+                  <span
+                    :class="activePath === route.path ? 'navbar-title active' : 'navbar-title'"
+                  >{{ $t(route.title) }}</span>
+                </template>
+                <b-dropdown-item
+                  class="dropdown-override"
+                  v-for="(item, index) in route.children"
+                  :key="index"
+                  :to="item.path"
+                  v-if="!item.hidden"
+                >
+                  <span v-if="!item.hidden">
+                    {{ $t(item.title) }}
+                    <!-- <b-dropdown-item class="dropdown-override" :to="subItem.path" v-for="(subItem, index) in item.children" :key="index">{{ $t(subItem.title) }}
+                    </b-dropdown-item>-->
+                  </span>
+                </b-dropdown-item>
+              </b-nav-item-dropdown>
+            </div>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
@@ -40,13 +72,18 @@ export default {
   },
   updated() {
     this.activePath = `/${this.$router.currentRoute.path.split('/')[1]}`;
-
-    
+  },
+  methods: {
+    toggleSidebar() {
+      window.isSidebarOn = !window.isSidebarOn;
+      this.isSidebarOn = window.isSidebarOn;
+    },
   },
   data() {
     return {
       routes,
       activePath: this.activePath,
+      isSidebarOn: false,
     };
   },
 };
@@ -73,7 +110,7 @@ export default {
   width: auto;
 }
 
-.main-nav-item{
+.main-nav-item {
   margin-right: 7px;
 }
 
@@ -86,4 +123,46 @@ export default {
   }
 }
 
+@media screen and (max-width: 992px) {
+  .navbar-collapse.collapse.show {
+    background-color: #081227;
+    height: unset;
+    width: 100%;
+    padding: 0 20px;
+  }
+
+  .navbar {
+    display: none;
+  }
+  .mobile {
+    display: block;
+  }
+
+  .sidebar {
+    width: 90%;
+    background: #fff;
+    position: fixed;
+    top: 79px;
+    left: 0;
+    height: 100%;
+    z-index: 1000000;
+    left: -90%;
+    transition: 0.3s ease all;
+  }
+
+  .sidebar.show {
+    box-shadow: 0 20px 20px 0 #00000066;
+    left: 0;
+    display: block;
+  }
+
+  .btn-group {
+    width: 100%;
+    text-align: left !important;
+  }
+
+  .btn-group > .btn.dropdown-toggle {
+    text-align: left !important;
+  }
+}
 </style>
