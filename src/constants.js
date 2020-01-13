b6d00f118b02f35aaa232ccfe01ce72db0ac855b
gkoroshinadze <gkoroshinadze@gmail.com>
2020-01-13 19:01:25 +0400

    releases

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = `http://admin.competition.ge:8080`;
+export const API_BASE_URL = `http://admin.competition.ge`;
 
 // Home
 export const SLIDERDATA_URL = `${API_BASE_URL}/slidaerdata`;
